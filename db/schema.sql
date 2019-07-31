e5a5a210823857b14fbec611b6b1b2c12e836199
Abigail Ettore <aettore1@fordham.edu>
2019-07-31 15:55:59 -0400

    edits

--- db/schema.sql
+++ db/schema.sql
@@ -4,9 +4,9 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE products (
+CREATE TABLE burgers (
 	id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(45) NULL,
-    devoured BOOLEAN,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
