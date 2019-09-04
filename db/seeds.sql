340f8f8c37a9c18a4f2670e60eba7c1ba150d89b
Abigail Ettore <aettore1@fordham.edu>
2019-09-04 10:29:09 -0400

    edits

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)  
+VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured)  
+VALUES ('Hamburger', false);
+INSERT INTO burgers (burger_name, devoured)  
+VALUES ('Turkey Burger', false);
