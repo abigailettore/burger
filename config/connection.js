05c85155be2852df3a14c20e8e1ea4a409844d5d
Abigail Ettore <aettore1@fordham.edu>
2019-09-04 19:36:26 -0400

    msql

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,13 @@
 // Connect Node to MySQL
+require("dotenv").config()
+
 var mysql = require("mysql");
 
     var connection = mysql.createConnection({
         host: "localhost",
-        port: 8080,
+        port: 3306,
         user: "root",
-        password: "MaeCorrigan8599",
+        password: process.env.password,
         database: "burgers_db"
     });
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var mysql = require("mysql");
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
